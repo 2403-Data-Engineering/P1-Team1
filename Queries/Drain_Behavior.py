7a5b1e563e1b0cdd45845a13f6085b0ed6238e96
unknown <guivilatoro@gmail.com>
2026-04-17 14:46:55 -0400

    Added drain behavior

---
+++ Queries/Drain_Behavior.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+from get_gds_connection import get_gds_connection
+
+
+
+def find_large_transfers_with_cashout_after():
+    with get_gds_connection() as gds:
+        gds.set_database("neo4j")
+
+        gds.run_cypher("""
+            MATCH (a:Account)-[t1:TRANSACTION]->(b:Account)-[t2:TRANSACTION]->(c:Account)
+            WHERE 
+                t2.step - t1.step <= 100
+                AND t2.step >= t1.step  
+                AND t1.amount > 50000
+                AND (t1.newbalanceOrig / CASE WHEN t1.oldbalanceOrg = 0 THEN 1 
+                    ELSE t1.oldbalanceOrg END) < 0.4  
+                AND t2.amount > 50000
+                AND (t2.newbalanceOrig / CASE WHEN t2.oldbalanceOrg = 0 THEN 1 
+                    ELSE t2.oldbalanceOrg END) < 0.4  
+            set a += {drain_behavior : true},
+                b += {drain_behavior : true},
+                c += {drain_behavior : true}
+            """)
+
