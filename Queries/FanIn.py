094cec7a12a44ce6889becca36fdd4b60235c775
jtmagri <jtmagri1@gmail.com>
2026-04-21 13:12:59 -0400

    FanIn.py

--- Queries/FanIn.py
+++ Queries/FanIn.py
@@ -1,1 +1,7 @@
-
+fan_in_query = """
+MATCH (a:Account)-[:TRANSACTION]->(b:Account)
+WITH b, count(DISTINCT a) AS fan_in_count
+RETURN b.id AS account_id, fan_in_count
+ORDER BY fan_in_count DESC
+LIMIT 25;
+"""
