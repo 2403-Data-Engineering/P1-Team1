555439aa9024282dd3bfd267e2b21f0e91d2aa59
jtmagri <jtmagri1@gmail.com>
2026-04-21 16:13:52 -0400

    Update FanOut.py

--- Queries/FanOut.py
+++ Queries/FanOut.py
@@ -1,7 +1,12 @@
-fan_out_query = """
-MATCH (a:Account)-[:TRANSACTION]->(b:Account)
-WITH a, count(DISTINCT b) AS fan_out_count
-RETURN a.id AS account_id, fan_out_count
-ORDER BY fan_out_count DESC
-LIMIT 25;
-"""
+MATCH (a:Account)-[t:TRANSACTION]->(b:Account)
+WITH a, count(DISTINCT b) AS fan_out_count, sum(t.amount) AS total_out_amount
+SET a.fan_out_count = fan_out_count
+SET a.total_out_amount = total_out_amount
+SET a.fan_out_flag = CASE
+    WHEN fan_out_count >= 3 AND total_out_amount > 50000 THEN 1
+    ELSE 0
+END
+WITH a
+WHERE a.fan_out_flag = 1
+RETURN a.id AS fraudulent_account_id, a.fan_out_count, a.total_out_amount, a.fan_out_flag
+ORDER BY a.total_out_amount DESC
